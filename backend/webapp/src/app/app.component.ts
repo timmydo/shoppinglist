2546b3d908f0d11654b84e77d0e13c732d126247
Timmy Douglas <timmy@timmyd.info>
2018-04-26 18:50:08 -0700

    add

--- backend/webapp/src/app/app.component.ts
+++ backend/webapp/src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { BackendService } from './backend.service';
 import { Observable } from 'rxjs/Observable';
 import { UserResponse } from './models';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   lists$: Observable<UserResponse>;
   title = 'app';
 
