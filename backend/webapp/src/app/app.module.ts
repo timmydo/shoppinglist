61bb7180b41fae7dd0db33f0d67c5afd065729fa
Timmy Douglas <timmy@timmyd.info>
2018-04-30 09:44:05 -0700

    add

--- backend/webapp/src/app/app.module.ts
+++ backend/webapp/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+import { MatButtonModule, MatCheckboxModule, MatToolbarModule } from '@angular/material';
 import { AppComponent } from './app.component';
 import { AuthService } from './auth.service';
 import { BackendService } from './backend.service';
@@ -18,7 +18,7 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    MatButtonModule, MatCheckboxModule
+    MatButtonModule, MatCheckboxModule, MatToolbarModule
   ],
   providers: [AuthService, BackendService, MessageService,
     {
